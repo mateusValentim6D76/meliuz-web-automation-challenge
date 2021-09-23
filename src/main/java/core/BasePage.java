a051773d4c1334873fce02e962893f48e7a876fe
Mateus Valentim <38231569+MateusValentim@users.noreply.github.com>
2021-09-23 13:04:40 -0300

    Adicionando ao carrinho

--- src/main/java/core/BasePage.java
+++ src/main/java/core/BasePage.java
@@ -1,11 +1,9 @@
 package core;
 
-import java.util.List;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
@@ -19,6 +17,7 @@ public class BasePage {
     private WebDriver driver;
     private WebDriverWait wait;
     private JavascriptExecutor js;
+    private static Actions actions;
     private PesquisaProdutoPage send;
    
 
@@ -27,6 +26,7 @@ public class BasePage {
         PageFactory.initElements(driver, this);
         wait = new WebDriverWait(this.driver,10);
         js = (JavascriptExecutor) this.driver;
+        actions = new Actions(driver);
     }
 
     @FindBy (how = How.ID, using = "nav_automobile")
@@ -64,4 +64,12 @@ public class BasePage {
     	wait.until(ExpectedConditions.visibilityOf(radioButtonPlatinum));
     	radioButtonPlatinum.click();
     }
+    
+    protected static void moveToElement(WebElement elemento) {
+		try {
+			actions.moveToElement(elemento);
+		} catch (Exception e) {
+			System.out.println("Nao foi possivel mover o mouse para o elemento");
+		}
+	}
 }
--- src/main/java/pageFactory/PesquisaProdutoPage.java
+++ src/main/java/pageFactory/PesquisaProdutoPage.java
@@ -2,44 +2,67 @@ package pageFactory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 import testDataType.PesquisaProdutoExistentePojo;
+import util.JsScroll;
 
 public class PesquisaProdutoPage {
 
 	private WebDriverWait wait;
 	private WebDriver driver;
+	private JsScroll js;
+	private static Actions actions;
 
 	public PesquisaProdutoPage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 		wait = new WebDriverWait(this.driver, 10);
+		js = new JsScroll(driver);
+		actions = new Actions(driver);
 
 	}
 
 	@FindBy(how = How.ID, using = "search_query_top")
 	private static WebElement inputBuscaProduto;
+	
+	@FindBy (how = How.CSS, using = "button[name= 'submit_search']")
+	private static WebElement botaoPesquisarProduto;
+	
+	@FindBy (how = How.ID, using = "list")
+	private static WebElement listarProdutos;
+	
+	@FindBy (how = How.LINK_TEXT, using = "Add to cart")
+	private static WebElement addCarrinho;
+	
 		
 	public void inputBuscaPorProduto(String buscarProduto) {
 		wait.until(ExpectedConditions.visibilityOf(inputBuscaProduto));
 		inputBuscaProduto.sendKeys(buscarProduto);
+		botaoPesquisarProduto.click();
 	}
 	
+	public void adicionarProdutoAoCarrinho() {
+		js.scrollDown();
+		listarProdutos.click();
+		actions.moveToElement(addCarrinho);	
+		addCarrinho.click();
+	}
+		
 //	public boolean loading() {
 //		wait.until(ExpectedConditions.visibilityOf(loading));
 //		return loading.isDisplayed();
 //
 //	}
-
 	
-	public void buscaProdutoExistenteAction(PesquisaProdutoExistentePojo produtoExistenteData) {
+	public void buscaProdutoExistenteAction(PesquisaProdutoExistentePojo produtoExistenteData) throws InterruptedException {
 		inputBuscaPorProduto(produtoExistenteData.pesquisaBlouse);
+		adicionarProdutoAoCarrinho();
 	}
 	
 }
